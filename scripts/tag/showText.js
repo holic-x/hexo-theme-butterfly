68b74c721fb3993823ed520294b75c4300426364
holic-x <43883789+holic-x@users.noreply.github.com>
2022-03-05 09:59:27 +0800

    theme update
    1.bber调整
    2.引入charts.js
    3.自定义标签调整

---
+++ scripts/tag/showText.js
@@ -0,0 +1,14 @@
+
+'use strict'
+
+// 定义函数
+// function addText (args, content) {
+//   const text = args[0] || 'non'
+
+//   console.log("showText Plugin");
+//   return `<h1>${text}</h1>`;
+
+// }
+
+// 注册插件
+// hexo.extend.tag.register('showText', addText, { ends: false })
